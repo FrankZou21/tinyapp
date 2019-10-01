c0629ee59eb3da5a981f854f6e7e967743e50b0b
Frank <frankzou2000@hotmail.com>
2019-10-01 22:17:00 +0000

    added logout function and logic

--- express_server.js
+++ express_server.js
@@ -19,16 +19,26 @@ function generateRandomString() {
 }
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"] 
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { 
+    username: req.cookies["username"] 
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -51,6 +61,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //Post for username
 app.post("/urls/login", (req, res) => {
   res.cookie("username", req.body.username);
+  res.redirect(`/urls`);
+});
+
+//Post for logging out
+app.post("/urls/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect(`/urls`);
 });
 
